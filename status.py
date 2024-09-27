9b2570b44ebbdb16aac25968e5f23d1e81f39962
James Tauber <jtauber@jtauber.com>
2024-09-26 20:17:49 -0400

    status script

---
+++ status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+DATA_DIR = Path("data")
+
+count = 0
+count_tagged = 0
+
+for group in DATA_DIR.iterdir():
+    for work in group.iterdir():
+        for file in work.iterdir():
+            if file.suffix == ".tsv" and ".tagged" not in file.stem:
+                count += 1
+                if file.with_suffix(".tagged.tsv").exists():
+                    count_tagged += 1
+
+print(f"{count_tagged} of {count} files tagged.")
